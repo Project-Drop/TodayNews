2e582458857a981e5453f3bd5531da972d82993a
Darshanhegdejaji <darshanhegdejaji@gmail.com>
2024-05-24 18:18:45 +0530

    signin and main activity done

--- app/src/main/java/com/example/todaynews/SignInActivity.kt
+++ app/src/main/java/com/example/todaynews/SignInActivity.kt
@@ -2,24 +2,72 @@ package com.example.todaynews
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.todaynews.databinding.ActivityMainBinding
 import com.example.todaynews.databinding.ActivitySignInBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.actionCodeSettings
+import com.google.firebase.auth.auth
 
 class SignInActivity : AppCompatActivity() {
+    private var auth = Firebase.auth
     private lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         binding.sign.setOnClickListener {
-            val intent = Intent(this, HomePageActivity::class.java)
-            startActivity(intent)
+            var email : String = binding.mail.text.toString()
+            var password : String = binding.password.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        val user = auth.currentUser
+                        if(user.toString().isNotEmpty()){
+                            if(user?.isEmailVerified != true){
+                                sendverificationEmail(user?.email.toString())
+                            }
+                            val intent = Intent(this,HomePageActivity::class.java).apply {
+                                Intent.FLAG_ACTIVITY_CLEAR_TOP
+                            }
+                            startActivity(intent)
+                        }
+                    } else {
+                        Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT,).show()
+                    }
+                }
+
         }
+    }
+    private fun sendverificationEmail(email:String){
+        val actionCodeSettings = actionCodeSettings {
+            // URL you want to redirect back to. The domain (www.example.com) for this
+            // URL must be whitelisted in the Firebase Console.
+            url = "https://www.example.com/finishSignUp?cartId=1234"
+            // This must be true
+            handleCodeInApp = true
+            setIOSBundleId("com.example.ios")
+            setAndroidPackageName(
+                "com.example.android",
+                true, // installIfNotAvailable
+                "12", // minimumVersion
+            )
+        }
+        Firebase.auth.sendSignInLinkToEmail(email, actionCodeSettings)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val alertDialog: AlertDialog? = null
+                    alertDialog?.setMessage("Email sent to your mail please check for authentication")
+                    alertDialog?.show()
+                }
+            }
     }
 }
